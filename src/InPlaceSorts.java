433d09512ecba3a8551c4c04e0d7bb8d9f040cd2
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-01-18 09:22:45 -0500

    bubbles

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -6,14 +6,29 @@ public class InPlaceSorts {
 	}
 	
 	public static void insertionSort(int [] list1) {
-		
+		for (int i = 0; i < list1.length; i++) {
+			
+
+		}
+				
 	}
 	
 	public static void selectionSort(double [] list1) {
-		
+		for (int i = 0; i < list1.length; i++) {
+			
+		}
 	}
 	
 	public static void bubbleSort(String [] list1) {
-		
+		//while ()
+		for (int i = 0; i < list1.length; i++) {
+			for (int j = 0; j < list1.length-i; j++) {
+				if (list1[j].length > list1[j+1].length) {
+					String y = list1[i];
+					list1[i] = list1[i+1];
+					list1[i+1] = y;
+				}	
+			}
 		}
+	}
 }
